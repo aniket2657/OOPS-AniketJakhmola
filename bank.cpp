739b091ac1d92366baff8f74bce27812fbf03b43
aniket2657 <aniketjakhmola496@gmail.com>
2025-02-05 06:40:27 -0800

    Add files via upload

--- bank.cpp
+++ bank.cpp
@@ -1,27 +1,3 @@
-Implement a Program in C++ by defining a class to represent a bank account.
-
-Include the following:
-
-Data Members
-
-Name of the depositor
-
-• Account number
-
-Type of account (Saving, Current etc.)
-
-Balance amount in the account
-
-Member Functions
-
-• To assign initial values
-
-• To deposit an amount
-
-To withdraw an amount after checking the balance
-
-To display name and balance
-
 #include <iostream>
 #include <string>
 using namespace std;
